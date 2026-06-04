271242ff2f9aab8233c51a2f970cc937e2e7d148
tusharx0809 <tusharbhatia0809@outlook.com>
2026-06-04 10:10:42 +0530

    added functionality to check if parking lot is full

--- phase1/OOPS/main.py
+++ phase1/OOPS/main.py
@@ -5,6 +5,7 @@ from models.Bike import Bike
 from models.Car import Car
 from models.Vehicle import Vehicle
 import os
+import random
 
 def main():
     with DatabaseConnection.PostgresConnection(os.getenv("DATABASE")) as connection:
@@ -12,31 +13,19 @@ def main():
             parking_lot = ParkingLot(4,15,connection)
             print("Parking Lot on database created")
             
-            # vehicles = [
-            #     Car("HRO3K4061"),
-            #     Bike("HRO3V0478"),
-            #     Car("DL3CAY8821"),
-            #     Bike("MH12RN4509"),
-            #     Car("KA03MM1142"),
-            #     Bike("HR26CT8831"),
-            #     Car("UP16BL0054"),
-            #     Bike("DL1SAB7710"),
-            #     Car("GJ01RE2290"),
-            #     Bike("KA51X3311"),
-            #     Car("MH02EF5567"),
-            #     Bike("UP32JZ4482"),
-            #     Car("WB02N8819"),
-            #     Bike("GJ03AW9950"),
-            #     Car("TS07HK1102"),
-            #     Bike("HR03AB1234")
-            # ]
+            vehicles = [
+                    Car(f"CAR{i:04d}") for i in range(30)
+                ] + [
+                    Bike(f"BIKE{i:04d}") for i in range(30)
+                ]
 
             # Loop through the list to enter each vehicle automatically
-            # for vehicle in vehicles:
-            #     parking_lot.enterVehicle(vehicle, connection)
-            #     print(f"{vehicle.get_vehicle_type()} [{vehicle.license_plate}] entered successfully.")
-
-            parking_lot.exitVehicle(Car("UP16BL0054"), connection)
+            for vehicle in vehicles:
+                if (parking_lot.enterVehicle(vehicle, connection)):
+                    print(f"{vehicle.get_vehicle_type()} [{vehicle.license_plate}] entered successfully.")
+                else:
+                    break
+            
 
         except connection.OperationalError as e:
             print("Error: {e}")
