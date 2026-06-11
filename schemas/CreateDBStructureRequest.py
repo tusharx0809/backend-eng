1e3db2b457d64199f56e50b6c61aa95c1d0f3a1d
tusharx0809 <tusharbhatia0809@outlook.com>
2026-06-11 16:34:05 +0530

    restructured

--- schemas/CreateDBStructureRequest.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-class ParkitLotStructureRequest(BaseModel):
-    floors: int = Field(gt=0)
-    number_of_parkings: int = Field(gt=0)
-
-class ParkingLotStructureResponse(BaseModel):
-    success: bool
-    message: str
